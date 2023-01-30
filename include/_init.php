a1f704ff12297733a8c56e26fd711b55ba816d73
BesokDev <jonathan.gredler@gmail.com>
2023-01-30 17:39:02 +0100

    Update _init.php

--- include/_init.php
+++ include/_init.php
@@ -1,10 +1,11 @@
 <?php
 
-// Connexion à la BDD avecPDO
+// Connexion à la BDD avec PDO
 try {
     $bdd = new PDO('mysql:host=localhost:3306;dbname=troc', 'root','', [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
 } catch (PDOException $exception) {
-    echo $exception->getMessage(); die("Veuillez créer la BDD nommée 'troc' pour continuer.");
+    echo $exception->getMessage();
+    die(". <b>Veuillez créer la BDD 'troc' pour continuer (importer fichier sql/troc.sql dans phpMyAdmin).</b>");
 }
 // Démarrage d'une session php, nécessaire pour la connexion
 session_start();
